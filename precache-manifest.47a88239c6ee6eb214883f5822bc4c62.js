cc7c7c4b22b706a5f6a5a3262c63b93f7235c228
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2020-03-18 11:56:46 -0700

    Updates

---
+++ precache-manifest.47a88239c6ee6eb214883f5822bc4c62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f162ece8fc0ecfcd90e46d2bcb71758",
+    "url": "/pokeapi-front-end/index.html"
+  },
+  {
+    "revision": "325e3a8040f61a14376e",
+    "url": "/pokeapi-front-end/static/css/main.853b68c4.chunk.css"
+  },
+  {
+    "revision": "9a720184325551f05714",
+    "url": "/pokeapi-front-end/static/js/2.524dafca.chunk.js"
+  },
+  {
+    "revision": "3b173ed738774b4bb64ac2b5ddb03f3b",
+    "url": "/pokeapi-front-end/static/js/2.524dafca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "325e3a8040f61a14376e",
+    "url": "/pokeapi-front-end/static/js/main.0ab5a04b.chunk.js"
+  },
+  {
+    "revision": "727575e1044201c2a378",
+    "url": "/pokeapi-front-end/static/js/runtime-main.02f6fd61.js"
+  }
+]);
