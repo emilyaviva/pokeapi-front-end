66f9346b65643ccb2e9d39aabc8e66b5bd10f8ef
Emily Aviva Kapor-Mater <emily@emilyaviva.com>
2020-03-18 22:40:54 -0700

    Test fetch() by mocking

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
+import waitForExpect from 'wait-for-expect'
+import fetchMock from 'fetch-mock-jest'
 
 import App from './'
 
 describe('<App />', () => {
-  it('is alive at application start', () => {
-    const app = shallow(<App />)
-    expect(app.find('button').exists()).toBeTruthy()
+  beforeEach(() => {
+    fetchMock.restore()
   })
 
-  // it('changes state on click', () => {
-  //   const app = mount(<App />)
-  //   app.find('button').simulate('click')
-  //   expect(app).not.toHaveState({ pokemon: [] })
-  // })
+  it('is alive at application start', () => {
+    const app = shallow(<App />)
+    expect(app.find({ children: 'Get Many Pokemon' }).exists()).toBeTruthy()
+  })
+
+  it('changes state on click', async () => {
+    const testPokemon = [
+      { name: 'test pokemon one', url: 'http://example.com/1' },
+      { name: 'test pokemon two', url: 'http://example.com/2' }
+    ]
+    fetchMock.getAny({ results: testPokemon })
+    const app = mount(<App />)
+    app.find({ children: 'Get Many Pokemon' }).simulate('click')
+    await waitForExpect(() => {
+      expect(app.state('pokemon')).toEqual(testPokemon)
+    })
+  })
 })
